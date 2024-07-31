c16e673a7e703e1290943185c8378dcd807ffa92
Jingwen Lin <jingwenlin@Jingwens-MacBook-Pro.local>
2024-07-31 12:58:26 -0700

    delete unuse code/comments

--- src/components/CheckoutForm/PaymentForm.jsx
+++ src/components/CheckoutForm/PaymentForm.jsx
@@ -6,12 +6,6 @@ import Review from './Review';
 
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
 
-// const PaymentForm = ({ checkoutToken, backStep }) => {
-//   if (!checkoutToken || !checkoutToken.live) {
-//     return <div>Loading...</div>;
-//   }
-
-
 const PaymentForm = ({ checkoutToken, nextStep, backStep, shippingData, onCaptureCheckout }) => {
   const handleSubmit = async (event, elements, stripe) => {
     event.preventDefault();
